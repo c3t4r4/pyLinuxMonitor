6ba96e75bb34f8e59837017a495b21fd2cd4ad8b
Glauco Garcia Cetara <neocetara@hotmail.com>
2024-02-01 16:50:57 -0300

    Update get info

--- python/linuxMonitor.py
+++ python/linuxMonitor.py
@@ -11,21 +11,27 @@ hostname = socket.gethostname()
 def get_system_info():
     # Coleta informações de CPU
     cpu_usage = psutil.cpu_percent(interval=1)
+    cpu_total = psutil.cpu_count(logical=True)  # Total de CPUs
     
     # Coleta informações de memória
     memory = psutil.virtual_memory()
     ram_usage = memory.percent
+    ram_total = memory.total / (1024**3)  # Convertendo bytes para GB
     
     # Coleta informações do disco (múltiplos discos podem ser adicionados)
-    disk_usage = psutil.disk_usage('/').percent
+    disk = psutil.disk_usage('/')
+    disk_usage = disk.percent
+    disk_total = disk.total / (1024**3)  # Convertendo bytes para GB
     
     return {
-        'id': config.unique_id,
-        'key': config.api_key,
+        'id': config.UNIQUE_ID,
         'hostname': hostname,
         'cpu_usage': cpu_usage,
+        'cpu_total': cpu_total,
         'ram_usage': ram_usage,
+        'ram_total': ram_total,
         'disk_usage': disk_usage,
+        'disk_total': disk_total,
         'timestamp': datetime.now().isoformat()
     }
 
